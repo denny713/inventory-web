149c0c0e48dc97da56cff0c25527d6e56d3158c4
Denny Afrizal <denny.afrizal713@gmail.com>
2021-08-03 11:53:14 +0700

    Add More Plugin And Dependencies

--- src/view/Login.js
+++ src/view/Login.js
@@ -1,5 +1,68 @@
-function Login(){
+import {
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    FormGroup,
+    Form,
+    Input,
+    InputGroupAddon,
+    InputGroupText,
+    InputGroup,
+    Col,
+  } from "reactstrap";
 
+function Login() {
+    return (
+        <>
+            <Col lg="5" md="7">
+                <Card className="bg-secondary shadow border-0">
+                    <CardHeader className="bg-transparent pb-5">
+                        <div className="text-muted text-center mt-2 mb-3">
+                            <small>Sign In</small>
+                        </div>
+                    </CardHeader>
+                    <CardBody className="px-lg-5 py-lg-5">
+                        <Form role="form">
+                            <FormGroup className="mb-3">
+                                <InputGroup className="input-group-alternative">
+                                    <InputGroupAddon addonType="prepend">
+                                        <InputGroupText>
+                                            <i className="ni ni-email-83" />
+                                        </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input
+                                        placeholder="Username"
+                                        type="email"
+                                        autoComplete="new-email"
+                                    />
+                                </InputGroup>
+                            </FormGroup>
+                            <FormGroup>
+                                <InputGroup className="input-group-alternative">
+                                    <InputGroupAddon addonType="prepend">
+                                        <InputGroupText>
+                                            <i className="ni ni-lock-circle-open" />
+                                        </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input
+                                        placeholder="Password"
+                                        type="password"
+                                        autoComplete="new-password"
+                                    />
+                                </InputGroup>
+                            </FormGroup>
+                            <div className="text-center">
+                                <Button className="my-4" color="primary" type="button">
+                                    Sign in
+                                </Button>
+                            </div>
+                        </Form>
+                    </CardBody>
+                </Card>
+            </Col>
+        </>
+    );
 }
 
 export default Login;
