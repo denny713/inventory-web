144e0bc6e626bcef44c11235ed2ed6911e0cab5a
Denny Afrizal <denny.afrizal713@gmail.com>
2021-08-03 09:06:49 +0700

    Add Login Page

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./view/Login";
+
+var routes = [
+    {
+        path: "/login",
+        name: "Login",
+        icon: "ni ni-key-25 text-info",
+        component: Login,
+        layout: "/auth",
+    }
+];
+
+export default routes;
